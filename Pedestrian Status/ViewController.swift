1955e055027c60d62a0a33d6231ca9fd0d8b0a02
Can S√ºrmeli <c_surmeli@icloud.com>
2015-09-17 17:10:24 +0300

    Initial Commit

---
+++ Pedestrian Status/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pedestrian Status
+//
+//  Created by Can on 17/09/15.
+//  Copyright © 2015 Can Sürmeli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
